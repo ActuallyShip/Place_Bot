af96972505e4dea1fd0e657d94f57d2648d82ea6
funkyhippo <funkyhippo@f-ck.me>
2022-04-04 01:18:01 -0500

    Update to version 21 in websocket message.

--- guyaplacebot.user.js
+++ guyaplacebot.user.js
@@ -126,7 +126,7 @@ function connectSocket() {
       duration: DEFAULT_TOAST_DURATION_MS,
     }).showToast();
     socket.send(JSON.stringify({ type: "getmap" }));
-    socket.send(JSON.stringify({ type: "brand", brand: "userscriptV20" }));
+    socket.send(JSON.stringify({ type: "brand", brand: "userscriptV21" }));
   };
 
   socket.onmessage = async function (message) {
